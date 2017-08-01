8147207dc00f10f7825428e8b47af2a96b30da97
Luis Felipe Mileo <mileo@kmee.com.br>
2017-08-01 16:33:44 -0300

    [REM] A necessidade de pre-instalar pacotes antes de rodar o buildout

--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e -x
 virtualenv2 -p python2 .
-bin/pip install -r requirements.txt
-./pip-update-requirements.sh
 bin/pip install -U zc.buildout pip
 printf "[buildout]\nextends = default.cfg\n" >> buildout.cfg
 bin/buildout
