795474248684b60d9623143980bff3fc804cdae5
Luis Felipe Mileo <mileo@kmee.com.br>
2017-08-14 17:37:55 -0300

    [REF] Removendo do pre-build instruções desnecessárias

--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e -x
-virtualenv -p python2 .
-bin/pip install -r requirements.txt
-./pip-update-requirements.sh
-bin/pip install -U zc.buildout pip
-printf "[buildout]\nextends = default.cfg\n" >> buildout.cfg
-bin/buildout
+git submodule init
+git submodule update
+virtualenv sandbox --no-setuptools --no-pip
+sandbox/bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools==33.1.1 pip zc.buildout
+./sandbox/bin/buildout "$@"
