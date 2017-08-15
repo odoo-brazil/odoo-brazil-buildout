a9a7332626b0d6c1c36ba5866a363fb938541f91
Luis Felipe Mileo <mileo@kmee.com.br>
2017-08-15 01:38:24 -0300

    [REM] Submodulo não esta sendo usado

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -e -x
-git submodule init
-git submodule update
 virtualenv sandbox --no-setuptools --no-pip
 sandbox/bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools==33.1.1 pip zc.buildout
 ./sandbox/bin/buildout "$@"
